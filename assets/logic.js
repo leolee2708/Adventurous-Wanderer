25fbaea33e4e7130f1c9c162b305f0fc77045312
Amanda Nelson <amandaln4311@gmail.com>
2021-01-07 13:51:16 -0500

    trying to fix api

--- assets/logic.js
+++ assets/logic.js
@@ -1,43 +1,17 @@
-$(document).ready (function() {
+$(document).ready(function () {
     // Global var
-    // let url =  "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=cafe|FFFF00";
-    // let apikey = "AIzaSyA_LwhQsJXpaTsL-C1ZQEYb93z7eP1Asog"
-    // let cityName;
-    // let queryURL = 
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    //   }).then(function(response){
-    //       console.log(response);
-    //   }
+    let url =  "http://www.mapquestapi.com/geocoding/v1/reverse";
+    let apikey = "6BECJhBC72w4mEBuvxsoGhC7r3fwaA09";
+    let cityName;
 
+    function randomCity() {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let queryURL = url + apikey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+    }
 })
