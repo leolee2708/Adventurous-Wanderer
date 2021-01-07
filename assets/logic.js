da309c10421a81b68fcaf8d63e2171c79b0a8827
Amanda Nelson <amandaln4311@gmail.com>
2021-01-07 11:20:15 -0500

    added some logic, added apikey and https

---
+++ assets/logic.js
@@ -0,0 +1,43 @@
+$(document).ready (function() {
+    // Global var
+    // let url =  "https://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=cafe|FFFF00";
+    // let apikey = "AIzaSyA_LwhQsJXpaTsL-C1ZQEYb93z7eP1Asog"
+    // let cityName;
+    // let queryURL = 
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   }).then(function(response){
+    //       console.log(response);
+    //   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
