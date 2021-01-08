2dec9f6565dee94c231f5a32abd2bb2e02f1fb09
Amanda Nelson <amandaln4311@gmail.com>
2021-01-08 11:05:03 -0500

    working on api

--- assets/logic.js
+++ assets/logic.js
@@ -1,17 +1,26 @@
-$(document).ready(function () {
+$(document).ready (function() {
     // Global var
-    let url =  "http://www.mapquestapi.com/geocoding/v1/reverse";
-    let apikey = "6BECJhBC72w4mEBuvxsoGhC7r3fwaA09";
-    let cityName;
+      // let url =  "https://maps.googleapis.com/maps/api/js?";
+      //  let apikey = "AIzaSyA_LwhQsJXpaTsL-C1ZQEYb93z7eP1Asog";
+      // let cityName;
+      let queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyA_LwhQsJXpaTsL-C1ZQEYb93z7eP1Asog&callback=initMap";
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response){
+            console.log(response);
+       }
+//     let map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: 28.538336, lng: -81.379234 },
+//     zoom: 8,
+//   });
+// }
+// let queryURL = "https://maps.googleapis.com/maps/api" + apikey
+// // ajax request
+// $.ajax({  url: 'https://api.allorigins.win/get?url=' + encodeURIComponent(queryURL),method: 'GET'}).then(function(data){console.log(data.contents)});
 
-    function randomCity() {
-
-    let queryURL = url + apikey;
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    });
-    }
+// initMap();
+}
 })
