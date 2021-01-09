ab39466c6364f102c1e2c2db36fe0cecb230995c
Amanda Nelson <amandaln4311@gmail.com>
2021-01-08 20:19:54 -0500

    took out extra link

--- assets/logic.js
+++ assets/logic.js
@@ -9,65 +9,34 @@ $(document).ready (function() {
     let countyside = ["44.4654° N, 72.6874° W", "56.6228° N, 6.0723° W", "8.5069° S, 115.2625° E", "54.0469° N, 6.1902° W", "", "", "", "", "", ""];
   
     function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat, lng},
-    zoom: 8,
-  });
-  }
-  let queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=burgers%20NewSmyrnaBeach&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBpsko6mY2gC8yhiv3pQsX0X2axGTXKrE0"
-// ajax request
-  $.ajax({  url: 'https://api.allorigins.win/get?url=' + encodeURIComponent(queryURL),method: 'GET'}).then(function(data){console.log(data.contents)});
-  initMap();
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 28.538336, lng: -81.379234 },
+        zoom: 8,
+      });
+    }
+    let queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=burgers%20NewSmyrnaBeach&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBpsko6mY2gC8yhiv3pQsX0X2axGTXKrE0"
+    // ajax request
+    $.ajax({  url: 'https://api.allorigins.win/get?url=' + encodeURIComponent(queryURL),method: 'GET'}).then(function(data){console.log(data.contents)});
+    initMap();
 
-})
+    //  var unirest = require("unirest");
 
-
-    // var unirest = require("unirest");
-
-    // var req = unirest("GET", "https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php");
+    //  var req = unirest("GET", "https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php");
     
-    // req.query({
-    //     "location": "New York"
-    // });
+    //  req.query({
+    //      "location": "New York"
+    //  });
     
-    // req.headers({
-    //     "x-rapidapi-key": "fe2e4d049cmshb5133f4f7dbb862p1c13b1jsn15281d51fcfa",
-    //     "x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
-    //     "useQueryString": true
-    // });
+    //  req.headers({
+    //      "x-rapidapi-key": "fe2e4d049cmshb5133f4f7dbb862p1c13b1jsn15281d51fcfa",
+    //      "x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
+    //      "useQueryString": true
+    //  });
     
     
     // req.end(function (res) {
-    //     if (res.error) throw new Error(res.error);
+    //      if (res.error) throw new Error(res.error);
     
     //     console.log(res.body);
-    // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //  });
+  })
