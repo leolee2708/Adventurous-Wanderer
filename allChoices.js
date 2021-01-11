80d109686b997493d660451d332c554002c44098
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-01-11 10:48:31 -0500

    Created all objects for the city array

--- allChoices.js
+++ allChoices.js
@@ -1,44 +1,147 @@
 
-var allDestinations =[
+var allDestinations = [
     {
-        name : "Koloa, Hawaii",
-        region : "beach",
-        lat : "21.8733° N",
-        long : "159.4542° W",
-        cost : "rich",
-        isCold : false ,
-        isHot : true,
-        activities : ["Live Music","Hiking", "Biking", "Water Adventure"],
+        name: "Koloa, Hawaii",
+        region: "beach",
+        lat: "21.8733° N",
+        long: "159.4542° W",
+        cost: "rich",
+        isCold: false,
+        isHot: true,
+        activities: ["Live Music", "Hiking", "Biking", "Water Adventure"]
     },
     {
-        name : "British Columbia, Canada" ,
-        region : "mountain",
-        lat : "50.1163° N",
-        long : "122.9574° W",
-        cost : "",
-        isCold : ,
-        isHot : ,
-        activities : [],
+        name: "British Columbia, Canada",
+        region: "mountain",
+        lat: "50.1163° N",
+        long: "122.9574° W",
+        cost: "",
+        isCold: ,
+        isHot: ,
+        activities: []
     },
     {
-        name : "" ,
-        region : "mountain",
-        lat : "50.1163° N",
-        long : "122.9574° W",
-        cost : "",
-        isCold : ,
-        isHot : ,
-        activities : [],
+        name: "",
+        region: "mountain",
+        lat: "50.1163° N",
+        long: "122.9574° W",
+        cost: "",
+        isCold: ,
+        isHot: ,
+        activities: []
     },
-         {
-        name: "Stowe, Vermont" ,
+    {
+        name: "Stowe, Vermont",
         region: "Countryside",
         lat: "44.46",
         long: "72.68",
         cost: "budget",
         isCold: true,
         isHot: true,
-        activities:["hike", "bike",]
+        activities: ["hike", "bike",]
     },
+    //CITY
+    {
+        name: "Paris, France",
+        region: "city",
+        lat: "48.8566° N",
+        long: "2.3522° E",
+        cost: "rich",
+        isCold: true,
+        isHot: false,
+        activities: ["shopping", "dance", "culture", "bike", "love"]
+    },
+    {
+        name: "Rome, Italy",
+        region: "city",
+        lat: "41.9028° N",
+        long: "12.4964° E",
+        cost: "rich",
+        isCold: true,
+        isHot: true,
+        activities: ["shopping", "culture", "bike", "love"]
+    },
+    {
+        name: "London, UK",
+        region: "city",
+        lat: "51.5074° N",
+        long: "0.1278° W",
+        cost: "budget",
+        isCold: true,
+        isHot: false,
+        activities: ["shopping", "dance", "culture", "bike", "love"]
+    },
+    {
+        name: "New York City, New York",
+        region: "city",
+        lat: "40.7128° N",
+        long: "74.0060° W",
+        cost: "budget",
+        isCold: true,
+        isHot: true,
+        activities: ["shopping", "dance", "mic", "bike"]
+    },
+    {
+        name: "Bangkok, Thailand",
+        region: "city",
+        lat: "13.7563° N",
+        long: "100.5018° E",
+        cost: "cheap",
+        isCold: false,
+        isHot: true,
+        activities: ["shopping", "dance", "culture", "bike"]
+    },
+    {
+
+        name: "Mong Kok, Hong Kong",
+        region: "city",
+        lat: "22.3193° N",
+        long: "114.1694° E",
+        cost: "cheap",
+        isCold: true,
+        isHot: true,
+        activities: ["shopping", "culture", "bike"]
+    },
+
+    {
+        name: "Dubai, UAE",
+        region: "city",
+        lat: "25.2048° N",
+        long: "55.2708° E",
+        cost: "rich",
+        isCold: true,
+        isHot: true,
+        activities: ["shopping", "dance", "bike", "love"]
+    },
+    {
+        name: "Moscow, Russia",
+        region: "city",
+        lat: "55.7558° N",
+        long: "37.6173° E",
+        cost: "rich",
+        isCold: true,
+        isHot: false,
+        activities: ["shopping", "dance", "culture", "bike"]
+    },
+    {
+        name: "Torshavn, Faroe Islands",
+        region: "city",
+        lat: "62.0107° N",
+        long: "6.7741° W",
+        cost: "rich",
+        isCold: true,
+        isHot: false,
+        activities: ["shopping", "dance", "culture", "hike", "bike", "love", "swim"]
+    },
+    {
+        name: "Amsterdam, Netherlands",
+        region: "city",
+        lat: "52.3676° N",
+        long: "4.9041° E",
+        cost: "budget",
+        isCold: true,
+        isHot: true,
+        activities: ["shopping", "dance", "culture", "bike",]
+    }
 
 ]
