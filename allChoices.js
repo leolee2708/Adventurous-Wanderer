67295ff205aeb39e8b58f48847ff7c848ba28379
shayampatel0 <shayampatel@knights.ucf.ed>
2021-01-10 22:35:26 -0500

    adding the object again

---
+++ allChoices.js
@@ -0,0 +1,33 @@
+var allDestinations =[
+    {
+        name : "Koloa, Hawaii",
+        region : "beach",
+        lat : "21.8733° N",
+        long : "159.4542° W",
+        cost : "rich",
+        isCold : false ,
+        isHot : true,
+        activities : ["Live Music","Hiking", "Biking", "Water Adventure"],
+    },
+    {
+        name : "British Columbia, Canada" ,
+        region : "mountain",
+        lat : "50.1163° N",
+        long : "122.9574° W",
+        cost : "",
+        isCold : ,
+        isHot : ,
+        activities : [],
+    },
+    {
+        name : "" ,
+        region : "mountain",
+        lat : "50.1163° N",
+        long : "122.9574° W",
+        cost : "",
+        isCold : ,
+        isHot : ,
+        activities : [],
+    }
+
+]
