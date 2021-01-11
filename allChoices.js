7ac84606c8047345a57b1deee483881b1b08e938
Amanda Nelson <amandaln4311@gmail.com>
2021-01-11 10:02:29 -0500

    started on object

---
+++ allChoices.js
@@ -0,0 +1,14 @@
+var allDestinations = [
+
+    {
+        name: "Stowe, Vermont" ,
+        region: "Countryside",
+        lat: "44.46",
+        long: "72.68",
+        cost: "budget",
+        isCold: true,
+        isHot: true,
+        activities:["hike", "bike",]
+    },
+
+]
