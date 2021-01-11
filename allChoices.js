a445a240c45477dedaaed67ab367c026a847039d
leolee2708 <leho.vietquan@gmail.com>
2021-01-11 11:16:07 -0500

    fixed duplicates issues

--- allChoices.js
+++ allChoices.js
@@ -1,5 +1,9 @@
 
 var allDestinations = [
+
+
+    //Beach starts
+
     {
         name: "Koloa, Hawaii",
         region: "beach",
@@ -8,39 +12,98 @@ var allDestinations = [
         cost: "rich",
         isCold: false,
         isHot: true,
-        activities: ["Live Music", "Hiking", "Biking", "Water Adventure"]
+        activities: ["mic", "hike", "bike", "swim"],
     },
+
     {
-        name: "British Columbia, Canada",
-        region: "mountain",
-        lat: "50.1163° N",
-        long: "122.9574° W",
-        cost: "",
-        isCold: ,
-        isHot: ,
-        activities: []
-    },
-    {
-        name: "",
-        region: "mountain",
-        lat: "50.1163° N",
-        long: "122.9574° W",
-        cost: "",
-        isCold: ,
-        isHot: ,
-        activities: []
-    },
-    {
-        name: "Stowe, Vermont",
-        region: "Countryside",
-        lat: "44.46",
-        long: "72.68",
-        cost: "budget",
+        name: "Salcombe, United Kingdom",
+        region: "beach",
+        lat: "50.2246° N",
+        long: "3.7846° W",
+        cost: "rich",
         isCold: true,
-        isHot: true,
-        activities: ["hike", "bike",]
+        isHot: false,
+        activities: ["mic", "hike", "bike", "swim"],
     },
-    //CITY
+    {
+        name: "Isla Harbour, Bahamas",
+        region: "beach",
+        lat: "25.5070° N",
+        long: "76.6321 W",
+        cost: "rich",
+        isCold: false,
+        isHot: true,
+        activities: ["hike", "bike", "swim", "culture"],
+
+    },
+    {
+        name: "Bora-Bora, French Polynesia",
+        region: "beach",
+        lat: "16.5414 S",
+        long: "151.7376 W",
+        cost: "rich",
+        isCold: false,
+        isHot: true,
+        activities: ["hike", "bike", "swim", "culture"],
+
+    },
+    {
+        name: "Rio de Janeiro, Brazil",
+        region: "beach",
+        lat: "22.9871 S",
+        long: "43.2048 W",
+        cost: "cheap",
+        isCold: false,
+        isHot: true,
+        activities: ["hike", "bike", "swim", "culture"],
+
+    },
+    {
+        name: "Biarritz, France",
+        region: "beach",
+        lat: "43.4832 N",
+        long: "1.5586 W",
+        cost: "cheap",
+        isCold: false,
+        isHot: true,
+        activities: ["hike", "bike", "swim", "culture", "shopping"],
+
+    },
+    {
+        name: "Queensland, Australia",
+        region: "beach",
+        lat: "16.7705 S",
+        long: "145.6717 E",
+        cost: "budget",
+        isCold: false,
+        isHot: true,
+        activities: ["hike", "bike", "swim", "culture"],
+
+    },
+    {
+        name: "Anse Union, Seychelles",
+        region: "beach",
+        lat: "4.3715 S",
+        long: "55.8272 E",
+        cost: "budget",
+        isCold: false,
+        isHot: true,
+        activities: ["mic", "hike", "bike", "swim", "culture"],
+
+    },
+    {
+        name: "Saint-Tropez, France",
+        region: "beach",
+        lat: "43.2677 N",
+        long: "6.6407 E",
+        cost: "budget",
+        isCold: false,
+        isHot: true,
+        activities: ["mic", "hike", "bike", "swim", "culture"],
+
+    },
+
+    //CITY starts
     {
         name: "Paris, France",
         region: "city",
@@ -142,6 +205,108 @@ var allDestinations = [
         isCold: true,
         isHot: true,
         activities: ["shopping", "dance", "culture", "bike",]
-    }
+    },
+    // countryside
+    {
+        name: "Stowe, Vermont",
+        region: "Countryside",
+        lat: "44.46",
+        long: "72.68",
+        cost: "budget",
+        isCold: true,
+        isHot: true,
+        activities: ["hike", "bike",]
+    },
+    {
+        name: "Isle of Mull, Scotland",
+        region: "Countryside",
+        lat: "56°37",
+        long: "76°04",
+        cost: "rich",
+        isCold: true,
+        isHot: false,
+        activities: ["hike", "bike", "swim", "shopping"]
+    },
+    {
+        name: "Bali, Indonesia",
+        region: "Countryside",
+        lat: "8.34",
+        long: "115.09",
+        cost: "rich",
+        isCold: false,
+        isHot: true,
+        activities: ["hike", "bike", "swim", "culture"]
+    },
+    {
+        name: "Carlingford, Ireland",
+        region: "Countryside",
+        lat: "54.04",
+        long: "6.19",
+        cost: "rich",
+        isCold: true,
+        isHot: false,
+        activities: ["hike", "bike", "swim"]
+    },
+    {
+        name: "Saratoga, Wyoming",
+        region: "Countryside",
+        lat: "41.45",
+        long: "106.80",
+        cost: "budget",
+        isCold: true,
+        isHot: true,
+        activities: ["hike", "bike", "swim", "romance", "culture"]
+    },
+    {
+        name: "Napa, California",
+        region: "Countryside",
+        lat: "38.29",
+        long: "122.28",
+        cost: "rich",
+        isCold: true,
+        isHot: true,
+        activities: ["hike", "bike", "romance", "culture"]
+    },
+    {
+        name: "Essex, United Kingdom",
+        region: "Countryside",
+        lat: "51.57",
+        long: "0.48",
+        cost: "rich",
+        isCold: true,
+        isHot: false,
+        activities: ["bike", "romance", "culture"]
+    },
+    {
+        name: "Cornwall, United Kingdom",
+        region: "Countryside",
+        lat: "50.26",
+        long: "5.05",
+        cost: "rich",
+        isCold: true,
+        isHot: false,
+        activities: ["hike", "bike", "romance", "culture", "swim"]
+    },
+    {
+        name: "Moselle Valley, Germany",
+        region: "Countryside",
+        lat: "50.40",
+        long: "7.60",
+        cost: "budget",
+        isCold: true,
+        isHot: false,
+        activities: ["hike", "bike", "romance", "culture"]
+    },
+    {
+        name: "Glasgow, Scotland",
+        region: "Countryside",
+        lat: "55.86",
+        long: "4.25",
+        cost: "rich",
+        isCold: true,
+        isHot: false,
+        activities: ["shopping", "romance", "culture"]
+    },
+
 
 ]
